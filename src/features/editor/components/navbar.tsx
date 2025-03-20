297322bfc0d682f86aa9e5c158896309b0e30325
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-03-20 15:57:51 +0530

    Add logo component and integrate it into the navbar

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { Logo } from "./logo"
+
 export const Navbar = () => {
   return (
     <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
-      Navbar
+      <Logo />
     </nav>
   )
 }
