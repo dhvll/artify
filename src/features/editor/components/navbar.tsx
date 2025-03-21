b8e48ce841a015d5dabddf0d717203b3ea00143b
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-03-21 23:17:02 +0530

    feat: update Navbar component to accept activeTool and onChangeActiveTool props

--- src/features/editor/components/navbar.tsx
+++ src/features/editor/components/navbar.tsx
@@ -20,8 +20,15 @@ import { CiFileOn } from "react-icons/ci"
 import { Separator } from "@/components/ui/separator"
 import { Hint } from "@/components/hint"
 import { BsCloudCheck, BsCloudSlash } from "react-icons/bs"
+import type { ActiveTool } from "../types"
+import { cn } from "@/lib/utils";
 
-export const Navbar = () => {
+interface NavbarProps {
+  activeTool: ActiveTool
+  onChangeActiveTool: (tool: ActiveTool) => void
+}
+
+export const Navbar = ({activeTool, onChangeActiveTool}: NavbarProps) => {
   return (
     <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
       <Logo />
@@ -50,7 +57,7 @@ export const Navbar = () => {
         </DropdownMenu>
         <Separator orientation="vertical" className="mx-2" />
         <Hint label="Select" side="bottom" sideOffset={10}>
-          <Button variant="ghost" size="icon" onClick={() => {}} className="">
+          <Button variant="ghost" size="icon" onClick={() => onChangeActiveTool("select")} className={cn(activeTool === "select" && "bg-gray-100")}>
             <MousePointerClick className="size-4" />
           </Button>
         </Hint>
