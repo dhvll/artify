54b83350b265228c870206e8c9461366641f3e8d
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-03-25 14:21:57 +0530

    feat: integrate FillColorSidebar into the editor component

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -9,6 +9,7 @@ import { Toolbar } from "./toolbar"
 import { Footer } from "./footer"
 import type { ActiveTool } from "../types"
 import { ShapeSidebar } from "./shape-sidebar"
+import { FillColorSidebar } from "./fill-color-sidebar"
 
 export const Editor = () => {
   const [activeTool, setActiveTool] = useState<ActiveTool>("select")
@@ -50,6 +51,7 @@ export const Editor = () => {
       canvas.dispose()
     }
   }, [init])
+
   return (
     <div className="h-full flex flex-col">
       <Navbar activeTool={activeTool} onChangeActiveTool={onChangeActiveTool} />
@@ -63,8 +65,18 @@ export const Editor = () => {
           activeTool={activeTool}
           onChangeActiveTool={onChangeActiveTool}
         />
+        <FillColorSidebar
+          editor={editor}
+          activeTool={activeTool}
+          onChangeActiveTool={onChangeActiveTool}
+        />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
-          <Toolbar />
+          <Toolbar
+            editor={editor}
+            activeTool={activeTool}
+            onChangeActiveTool={onChangeActiveTool}
+            key={JSON.stringify(editor?.canvas.getActiveObject())}
+          />
           <div
             className="flex-1 h-[calc(100%-124px)] bg-muted"
             ref={containerRef}
